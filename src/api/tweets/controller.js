5b292c12e49aeb37ca4ec62a0e80a228f5609047
Suyash Mohan <suyashatweb@gmail.com>
2017-09-16 18:16:29 +0530

    Sorting & Limit added to the /tweets api

--- src/api/tweets/controller.js
+++ src/api/tweets/controller.js
@@ -2,7 +2,16 @@ const TweetModel = require('./model.js');
 
 const controller = {
     getAll: (req, res) => {
-        TweetModel.find().exec()
+        const limit = parseInt(req.query.limit) || 10;
+        const sortQuery = {};
+        if (req.query.sort !== undefined) {
+            sortQuery.retweetCount = -1;
+        }
+
+        TweetModel.find()
+            .limit(limit)
+            .sort(sortQuery)
+            .exec()
             .then((result) => {
                 res.status(200).json({
                     result
